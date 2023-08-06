77ef3cd0d73ea1b634b43f647c7b8b7b45e54e13
AiutoPcAmico <andrea@andreafelappi.it>
2023-08-06 17:31:37 +0200

    Modified addReservation with new prices

--- functions/reservation/reservationPut.js
+++ functions/reservation/reservationPut.js
@@ -22,29 +22,10 @@ async function addCompleteReservation(resDetail, foods, beverages) {
     };
   }
 
-  //calculating total price
-
-  //getting price of beverages
-  var beveragePrice = 0;
-  if (beverages && beverages.length > 0) {
-    var formattedBeveragesIDs = beverages.map((el) => el.idBeverage);
-    beveragePrice = await getPriceOfBeverages(formattedBeveragesIDs);
-    beveragePrice = parseFloat(beveragePrice[0].totalPrice);
-  }
-  //getting price of foods
-  var foodPrice = 0;
-  if (foods && foods.length > 0) {
-    var formattedFoodsIDs = foods.map((el) => el.idFood);
-    foodPrice = await getPriceOfFoods(formattedFoodsIDs);
-    foodPrice = parseFloat(foodPrice[0].totalPrice);
-  }
-  //at last, the total price
-  const totalPrice = beveragePrice + foodPrice;
-
   //preparing sql statements
-  var sqlAddReservation = `INSERT INTO prenotazione (tavolo, coperti, nominativo, isPagato, totale) VALUES (?,?,?,?,?);`;
-  var sqlAddBeverages = `INSERT INTO erogazione (idPrenotazione, idBevanda, quantita, noteErogazione) VALUES ?;`;
-  var sqlAddFoods = `INSERT INTO preparazione (idPrenotazione, idCibo, quantita, notePreparazione) VALUES ?;`;
+  var sqlAddReservation = `INSERT INTO prenotazione (tavolo, coperti, nominativo, isPagato) VALUES (?,?,?,?);`;
+  var sqlAddBeverages = `INSERT INTO erogazione (idPrenotazione, idBevanda, quantita, noteErogazione, prezzoSingoloPagato) VALUES ?;`;
+  var sqlAddFoods = `INSERT INTO preparazione (idPrenotazione, idCibo, quantita, notePreparazione, prezzoSingoloPagato) VALUES ?;`;
 
   // starting queries
   try {
@@ -57,66 +38,73 @@ async function addCompleteReservation(resDetail, foods, beverages) {
 
   //creating the reservation
   var resultReservation = null;
-  try {
-    resultReservation = await dbSagre
-      .promise()
-      .query(sqlAddReservation, [
-        resDetail.table,
-        resDetail.coverCharge,
-        resDetail.name,
-        resDetail.isPayed,
-        totalPrice,
-      ]);
-  } catch (err) {
-    result.error = true;
-    result.data = `${err.message}\nError while saving reservation details`;
-    result.status = 400;
-  }
+  // try {
+  resultReservation = await dbSagre
+    .promise()
+    .query(sqlAddReservation, [
+      resDetail.table,
+      resDetail.coverCharge,
+      resDetail.name,
+      resDetail.isPayed,
+    ]);
+  //} catch (err) {
+  //  result.error = true;
+  //  result.data = `${err.message}\nError while saving reservation details`;
+  //  result.status = 400;
+  //}
 
   //check if required beverages
   if (!result.error && beverages && beverages.length > 0) {
-    const formattedBeverages = beverages.map((single) => {
-      return [
-        resultReservation[0].insertId,
-        single.idBeverage,
-        single.quantity,
-        single.notes,
-      ];
-    });
+    const formattedBeverages = await Promise.all(
+      beverages.map(async (single) => {
+        const price = await getPriceOfBeverages([single.idBeverage]);
+        return [
+          resultReservation[0].insertId,
+          single.idBeverage,
+          single.quantity,
+          single.notes,
+          price[0].totalPrice,
+        ];
+      })
+    );
 
     //creating requests of beverages
-    try {
-      const resultBeverages = await dbSagre
-        .promise()
-        .query(sqlAddBeverages, [formattedBeverages]);
-    } catch (err) {
-      result.error = true;
-      result.data = `${err.message}\nError while saving beverages! Rollback actions!`;
-      result.status = 400;
-    }
+    // try {
+    const resultBeverages = await dbSagre
+      .promise()
+      .query(sqlAddBeverages, [formattedBeverages]);
+    // } catch (err) {
+    //   result.error = true;
+    //   result.data = `${err.message}\nError while saving beverages! Rollback actions!`;
+    //   result.status = 400;
+    // }
   }
 
   //check if required food
   if (!result.error && foods && foods.length > 0) {
-    const formattedFoods = foods.map((single) => {
-      return [
-        resultReservation[0].insertId,
-        single.idFood,
-        single.quantity,
-        single.notes,
-      ];
-    });
+    const formattedFoods = await Promise.all(
+      foods.map(async (single) => {
+        const price = await getPriceOfFoods([single.idFood]);
+        return [
+          resultReservation[0].insertId,
+          single.idFood,
+          single.quantity,
+          single.notes,
+          price[0].totalPrice,
+        ];
+      })
+    );
 
     //creating requests of foods
-    try {
-      const resultFoods = await dbSagre
-        .promise()
-        .query(sqlAddFoods, [formattedFoods]);
-    } catch (err) {
-      result.error = true;
-      result.data = `${err.message}\nError while saving foods! Rollback actions!`;
-      result.status = 400;
-    }
+    //  try {
+    const resultFoods = await dbSagre
+      .promise()
+      .query(sqlAddFoods, [formattedFoods]);
+    //  } catch (err) {
+    //    result.error = true;
+    //    result.data = `${err.message}\nError while saving foods! Rollback actions!`;
+    //    result.status = 400;
+    //  }
   }
 
   if (result.error === false) {
